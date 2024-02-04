093dfaed350a3e8cc9999ea60ecf6980d97baadd
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2024-02-04 15:27:21 -0700

    adding mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+    console.log(`MongoDB is connected.`)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 app.get("/", (req, res) => {
     res.send("You are pinging the Express Server.")
@@ -8,4 +20,4 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
     console.log(`Server is up and listening on port 3000`)
-})
+}) 
